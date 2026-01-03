6d29d701ab4a07d66a975cc087b7bdc8e3c2b6c7
RSD6170 <rsd6170+github@mailbox.org>
2026-01-03 18:34:26 +0100

    feat: add all data provider

--- ml_analysis/execution/plugin_index.py
+++ ml_analysis/execution/plugin_index.py
@@ -1,10 +1,26 @@
+import typing
+
 from interfaces.data_provider import DataProvider
-from plugins.data_provider.fm_benchmark_solver import SATRuntime
+from plugins.data_provider.fm_benchmark_solver import SATRuntime, BackboneRuntime, SpurRuntime, SSATValue, \
+    BackboneValue, AlgoSelection
 
 
-def get_data_provider(name:str) -> type[SATRuntime]:
+def get_data_provider(name:str) -> typing.Type[DataProvider]:
     match name:
         case "runtime_sat":
             return SATRuntime
+        case "runtime_backbone":
+            return BackboneRuntime
+        case "runtime_spur":
+            return SpurRuntime
+        case "value_ssat":
+            return SSATValue
+        case "value_backbone":
+            return BackboneValue
+        case "algo_selection":
+            return AlgoSelection
         case _:
             raise NotImplementedError(f"Data provider \"{name}\" not implemented!")
+
+
+def get_
--- ml_analysis/plugins/data_provider/fm_benchmark_solver.py
+++ ml_analysis/plugins/data_provider/fm_benchmark_solver.py
@@ -1,9 +1,24 @@
 import pathlib
+from abc import ABC
+from collections import Counter
 
 import numpy as np
 import pandas as pd
 
 from interfaces.data_provider import DataProvider, LabelType
+
+_sharpsat_names = [
+        "approxmc",
+        "countantom",
+        "d4v2_23",
+        "d4v2_24",
+        "exactmc_arjun",
+        "ganak",
+        "sharpsattd"
+    ]
+
+_suffix_mc = "_modelCount"
+_suffix_time = "_wallclockTimeS"
 
 def load_dataset(path : pathlib.Path, subpath : str) -> pd.DataFrame:
     df = pd.read_csv(path / subpath, header=0, low_memory=False, index_col="modelNo")
@@ -17,14 +32,20 @@ def load_dataset(path : pathlib.Path, subpath : str) -> pd.DataFrame:
     df.replace([np.inf, -np.inf], np.nan, inplace=True)
     return df
 
-def load_fm_bench_data(path: str) -> pd.DataFrame:
+def load_fm_bench_data(path: pathlib.Path) -> pd.DataFrame:
     df = load_dataset(path, "featureExtraction/values.csv")
     df.columns = df.columns.map(str)
     df.replace([np.inf, -np.inf], np.nan, inplace=True)
     return df
 
-class SATRuntime(DataProvider):
+class FMBenchProvider(DataProvider, ABC):
+    def _remove_feature_target_duplicates(self, data):
+        return data
 
+    def _load_data(self, data_root: pathlib.Path):
+        return load_fm_bench_data(data_root)
+
+class SATRuntime(FMBenchProvider):
     def _load_labels(self, data_root: pathlib.Path):
         df = load_dataset(data_root, "runtime/sat.csv")
         return df.loc[:, 'wallclockTimeS'].astype(float)
@@ -32,9 +53,83 @@ class SATRuntime(DataProvider):
     def get_label_type(self) -> LabelType:
         return LabelType.NUMERIC
 
+
+class BackboneRuntime(FMBenchProvider):
+    def _load_labels(self, data_root: pathlib.Path):
+        df = load_dataset(data_root, "runtime/backbone.csv")
+        return df.loc[:, 'wallclockTimeS'].astype(float)
+
+    def get_label_type(self) -> LabelType:
+        return LabelType.NUMERIC
+
+
+class SpurRuntime(FMBenchProvider):
+    def _load_labels(self, data_root: pathlib.Path):
+        df = load_dataset(data_root, f"runtime/spur_100.csv")
+        return df.loc[:, 'wallclockTimeS'].astype(float)
+
+    def get_label_type(self) -> LabelType:
+        return LabelType.NUMERIC
+
+
+class SSATValue(FMBenchProvider):
+    @staticmethod
+    def check_ssat_value(row: pd.Series) -> int:
+        row_na = row.dropna()
+        counts = [int(row[f"{i}{_suffix_mc}"]) for i in _sharpsat_names if f"{i}{_suffix_mc}" in row_na.index]
+        if len(counts) == 0:
+            return -1
+        else:
+            return Counter(counts).most_common(1)[0][0]
+
+    def _load_labels(self, data_root: pathlib.Path):
+        df = load_dataset(data_root, "runtime/sharpsat.csv")
+        return df.apply(self.check_ssat_value, axis=1)
+
+    def get_label_type(self) -> LabelType:
+        return LabelType.NUMERIC
+
     def _remove_feature_target_duplicates(self, data):
-        return data
+        return data.drop(
+            ['DyMMer_9/DyMMer/Number_of_valid_configurations_(NVC)', 'FMBA_18/FMBA/NumberOfValidConfigurationsLog',
+             'FM_Characterization_1/FM_Characterization/ANALYSIS/Configurations/value',
+             'FM_Characterization_1/FM_Characterization/ANALYSIS/Partial_variability/value',
+             'FM_Characterization_1/FM_Characterization/ANALYSIS/Total_variability/value'], axis=1)
 
-    def _load_data(self, data_root: pathlib.Path):
-        return load_fm_bench_data(data_root)
+class BackboneValue(FMBenchProvider):
+    def _load_labels(self, data_root: pathlib.Path):
+        df = load_dataset(data_root, "runtime/backbone.csv")
+        df = df.fillna(-1)
+        return df.loc[:, 'backboneSize'].astype(int)
 
+    def get_label_type(self) -> LabelType:
+        return LabelType.NUMERIC
+
+class AlgoSelection(FMBenchProvider):
+
+    @staticmethod
+    def check_ssat_value(row: pd.Series) -> int:
+        row_na = row.dropna()
+        counts = [int(row[f"{i}{_suffix_mc}"]) for i in _sharpsat_names if f"{i}{_suffix_mc}" in row_na.index]
+        if len(counts) == 0:
+            return -1
+        else:
+            return Counter(counts).most_common(1)[0][0]
+
+    @staticmethod
+    def check_best_sSAT_solver(row: pd.Series) -> str:
+        count = AlgoSelection.check_ssat_value(row)
+        row_na = row.dropna()
+        if count == -1:
+            time_dict = {solver: row[solver + _suffix_time] for solver in _sharpsat_names}
+        else:
+            time_dict = {solver: row[solver + _suffix_time] for solver in _sharpsat_names if
+                         f"{solver}{_suffix_mc}" in row_na.index and int(row[solver + _suffix_mc]) == count}
+        return min(time_dict, key=time_dict.get)
+
+    def _load_labels(self, data_root: pathlib.Path):
+        df = load_dataset(data_root, "runtime/sharpsat.csv")
+        return df.apply(AlgoSelection.check_best_sSAT_solver, axis=1)
+
+    def get_label_type(self) -> LabelType:
+        return LabelType.CLASS
